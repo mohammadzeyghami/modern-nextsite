689fd0da65c1a5c720d2de2b4200d30372ab9ea8
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-02 22:53:42 +0100

    more changes

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,6 +1,8 @@
 "use client";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { headset } from "../public";
+
 import styles from "../styles";
 import { fadeIn } from "../utils/motion";
 const ExploreCard = ({ id, imgUrl, title, index, active, handelClick }) => (
@@ -10,7 +12,24 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handelClick }) => (
       active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
   >
-    Explore Card
+    <Image
+      src={imgUrl}
+      alt=""
+      className="absolute w-full h-full object-cover rounded-[20px]"
+    />
+    {active !== id ? (
+      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
+        {title}
+      </h3>
+    ) : (
+      <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounderd-b-[24px]">
+        <div
+          className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
+        >
+          <Image src={headset} className="w-1/2 h-1/2 object-contain mx-auto" />
+        </div>
+      </div>
+    )}
   </motion.div>
 );
 
--- constants/index.js
+++ constants/index.js
@@ -1,88 +1,99 @@
+import { planet01 } from "../public";
+import { planet02 } from "../public";
+import { planet03 } from "../public";
+import { planet04 } from "../public";
+import { planet05 } from "../public";
+import { planet06 } from "../public";
+import { planet07 } from "../public";
+import { planet08 } from "../public";
+import { planet09 } from "../public";
+import { vrpano } from "../public";
+import { headset } from "../public/headset.svg";
 export const exploreWorlds = [
   {
-    id: 'world-1',
-    imgUrl: '/planet-01.png',
-    title: 'The Hogwarts',
+    id: "world-1",
+    imgUrl: planet01,
+    title: "The Hogwarts",
   },
   {
-    id: 'world-2',
-    imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
+    id: "world-2",
+    imgUrl: planet02,
+    title: "The Upside Down",
   },
   {
-    id: 'world-3',
-    imgUrl: '/planet-03.png',
-    title: 'Kadirojo Permai',
+    id: "world-3",
+    imgUrl: planet03,
+    title: "Kadirojo Permai",
   },
   {
-    id: 'world-4',
-    imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
+    id: "world-4",
+    imgUrl: planet04,
+    title: "Paradise Island",
   },
   {
-    id: 'world-5',
-    imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
+    id: "world-5",
+    imgUrl: planet05,
+    title: "Hawkins Labs",
   },
 ];
 
 export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
-  'No need to beat around the bush, just stay on the gas and have fun',
+  "Find a world that suits you and you want to enter",
+  "Enter the world by reading basmalah to be safe",
+  "No need to beat around the bush, just stay on the gas and have fun",
 ];
 
 export const newFeatures = [
   {
-    imgUrl: '/vrpano.svg',
-    title: 'A new world',
+    imgUrl: vrpano,
+    title: "A new world",
     subtitle:
-        'we have the latest update with new world for you to try never mind',
+      "we have the latest update with new world for you to try never mind",
   },
   {
-    imgUrl: '/headset.svg',
-    title: 'More realistic',
+    imgUrl: headset,
+    title: "More realistic",
     subtitle:
-        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+      "In the latest update, your eyes are narrow, making the world more realistic than ever",
   },
 ];
 
 export const insights = [
   {
-    imgUrl: '/planet-06.png',
-    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    imgUrl: planet06,
+    title: "The launch of the Metaverse makes Elon musk ketar-ketir",
     subtitle:
-        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+      "Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.",
   },
   {
-    imgUrl: '/planet-07.png',
-    title: '7 tips to easily master the madness of the Metaverse',
+    imgUrl: planet07,
+    title: "7 tips to easily master the madness of the Metaverse",
     subtitle:
-        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+      "Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum",
   },
   {
-    imgUrl: '/planet-08.png',
-    title: 'With one platform you can explore the whole world virtually',
+    imgUrl: planet08,
+    title: "With one platform you can explore the whole world virtually",
     subtitle:
-        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+      "Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem",
   },
 ];
 
 export const socials = [
   {
-    name: 'twitter',
-    url: '/twitter.svg',
+    name: "twitter",
+    url: "/twitter.svg",
   },
   {
-    name: 'linkedin',
-    url: '/linkedin.svg',
+    name: "linkedin",
+    url: "/linkedin.svg",
   },
   {
-    name: 'instagram',
-    url: '/instagram.svg',
+    name: "instagram",
+    url: "/instagram.svg",
   },
   {
-    name: 'facebook',
-    url: '/facebook.svg',
+    name: "facebook",
+    url: "/facebook.svg",
   },
 ];
