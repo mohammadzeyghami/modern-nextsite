2a9b3dc2ee75b66840ffc029081c65b362f72564
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-02 19:38:26 +0100

    expolre

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,9 +1,17 @@
-'use client';
-
-const ExploreCard = () => (
-  <div>
+"use client";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { fadeIn } from "../utils/motion";
+const ExploreCard = ({ id, imgUrl, title, index, active, handelClick }) => (
+  <motion.div
+    variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+    className={`relative ${
+      active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
+    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+  >
     Explore Card
-  </div>
+  </motion.div>
 );
 
 export default ExploreCard;
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -5,27 +5,41 @@ import { exploreWorlds } from "../constants/index";
 import styles from "../styles";
 import { TypingText, ExploreCard, TitleText } from "../components";
 import { staggerContainer } from "../utils/motion";
-const Explore = () => (
-  <section className={`${styles.paddings}`} id="explore">
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    >
-      <TypingText title="| The World" textStyles="text-center" />
-      <TitleText
-        title={
-          <>
-            Choose the world you want
-            <br className="md:block hidden" /> to explore
-          </>
-        }
-        textStyles="text-center"
-      />
-    </motion.div>
-  </section>
-);
+const Explore = () => {
+  const [active, setActive] = useState("world-2");
+  return (
+    <section className={`${styles.paddings}`} id="explore">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Choose the world you want
+              <br className="md:block hidden" /> to explore
+            </>
+          }
+          textStyles="text-center"
+        />
+        <div className="flex flex-col lg:flex-row min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              handelClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Explore;
