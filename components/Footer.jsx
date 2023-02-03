1af929e95f1e54aaf112e31e7dac2b13149d6676
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-03 20:15:16 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -28,15 +28,32 @@ const Footer = () => (
             alt="headset"
             className="w-[24px] h-[24px] object-contain"
           />
-          <span className="font-normal text-[16px]">Enter The Metaverse</span>
-          <div className="flex flex-col">
-            <div className="mb-[50px] h-[2px] bg-white opacity-10">
-              <div className="flex items-center justify-between flex-wrap gap-4">
-                <h4>Metaversus</h4>
+        </button>
+        <span className="font-normal text-[16px]">Enter The Metaverse</span>
+        <div className="flex flex-col">
+          <div className="mb-[50px] h-[2px] bg-white opacity-10">
+            <div className="flex items-center justify-between flex-wrap gap-4">
+              <h4 className="font-extrabold text-[24px] text-[white]">
+                Metaversus
+              </h4>
+              <p className="font-normal text-[14px] text-white opacity-50">
+                Copyright © 2021 - 2022 Metaversus. All rights reserved.
+              </p>
+              <div className="flex gap-4">
+                {socials.map((social) => (
+                  <Image
+                    key={social.name}
+                    src={social.url}
+                    alt={social.name}
+                    width={24}
+                    height={24}
+                    className="w-[24px] h-[24px] object-contain cursor-pointer"
+                  />
+                ))}
               </div>
             </div>
           </div>
-        </button>
+        </div>
       </div>
     </div>
   </motion.footer>
