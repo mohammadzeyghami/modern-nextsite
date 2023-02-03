be6e18034ae11bf9b1b659709c0ab953a36919e9
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-03 02:11:41 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,18 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
+import { socials } from "../constants";
 
+import styles from "../styles";
+import { footerVariants } from "../utils/motion";
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.paddings}py-8 relative`}
+  >
+    <div className="footer-gradient" />
+  </motion.footer>
 );
 
 export default Footer;
